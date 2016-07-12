8aba35206c128d57172c086d23ca9e397a2acbc1
virtyaluk <winsatana@ya.ru>
2016-07-12 13:08:46 +0300

    Adds Task model

---
+++ api/models/Task.js
@@ -0,0 +1,19 @@
+/**
+ * Task.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    title: {
+      type: 'string',
+      required: true
+    },
+    completed: {
+      type: 'boolean',
+      defaultsTo: false
+    }
+  }
+};
